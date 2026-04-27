fd3a4a03019eba7078d4f07ff1325ed38c27eaf9
David Arorote <aroroted@gmail.com>
2026-04-27 20:15:42 +0000

    Added a configuration block to handle multiple files

--- models/staging/stg_data.sql
+++ models/staging/stg_data.sql
@@ -1,3 +1,16 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='user_id'
+  )
+}}
+
+select * from renamed
+{% if is_incremental() %}
+  -- only look at files added since the last dbt run
+  where dbt_updated_at > (select max(dbt_updated_at) from {{ this }})
+{% endif %}
+
 -- Rename 'google_drive_raw.your_table_name' to your actual source table name
 with source as (
     select * from {{ source('google_drive_raw', 'your_table_name') }}
